8779c46660a8535988f8e7b0bb69edcb49029a2d
mariahiguera2210 <angelica.higueram92@gmail.com>
2024-02-20 11:35:44 -0500

    se hacen las cards con angular material

---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.scss']
+})
+export class CardsComponent {
+
+  cards = [
+    {
+      title: 'Card 1',
+      img: 'https://picsum.photos/200/300',
+      desc: 'Description 1'
+    },
+    {
+      title: 'Card 2',
+      img: 'https://picsum.photos/200/300',
+      desc: 'Description 2'
+    },
+    {
+      title: 'Card 3',
+      img: 'https://picsum.photos/200/300',
+      desc: 'Description 3'
+    },
+    {
+      title: 'Card 4',
+      img: 'https://picsum.photos/200/300',
+      desc: 'Description 4'
+    }
+  ]
+
+}
+
